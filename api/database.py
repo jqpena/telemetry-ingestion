a56d0689442f388aff0ab00c37a6226f8844afe7
Julio Ernesto Quintero <jequinterope@unal.edu.co>
2026-03-15 22:48:43 -0500

    refactor: update entrypoint
    Takeout the entrypoint contradict with the usage an intented execution
    as a module. does not have sense to use it like that, added
    standard logging format for all involved loggers in the application

--- api/database.py
+++ api/database.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
@@ -7,6 +7,9 @@ from sqlalchemy.schema import MetaData
 
 from .config import Config
 from .exc import InternalError
+
+if TYPE_CHECKING:
+    from sqlalchemy import Engine
 
 
 class DBBase(DeclarativeBase):
@@ -18,34 +21,47 @@ class DBBase(DeclarativeBase):
         }
 
 
-_engine = create_engine(
-    Config["DB_URL"],
-    echo=True,
-    echo_pool=Config["LOG_LEVEL"],
-    isolation_level="READ COMMITTED",
-    pool_size=Config["DB_POOL_SIZE"],
-    max_overflow=15,
-    connect_args={"options": f"-c timezone={Config['TIMEZONE'].key or 'UTC'}"},
-)
+_engine: "Engine | None" = None
 
-SessionFactory = sessionmaker(bind=_engine, expire_on_commit=False, autoflush=False)
+SessionFactory: "sessionmaker | None" = None
 
-PG_VERSION = _engine.dialect.server_version_info
 PG_MINIMUM_VERSION = (18, 0)
-if not PG_VERSION:
-    try:
-        with _engine.connect() as conn:
-            version = conn.execute(text("SHOW server_version")).first()
-            PG_VERSION = version and tuple(int(x) for x in version[0].split("."))
-    except SQLAlchemyError:
-        PG_VERSION = (18, 0)  # Assume the minimum supported version if we can't determine it
-elif PG_VERSION < PG_MINIMUM_VERSION:
-    raise RuntimeError(
-        f"PostgreSQL version {PG_VERSION} is not supported. Upgrade to a newer version to use this application."
+
+
+def create_factories() -> "Engine":
+    engine = create_engine(
+        Config["DB_URL"],
+        echo=True,
+        echo_pool=Config["LOG_LEVEL"].lower() if Config["LOG_LEVEL"].lower() == "debug" else True,
+        isolation_level="READ COMMITTED",
+        pool_size=Config["DB_POOL_SIZE"],
+        max_overflow=15,
+        connect_args={"options": f"-c timezone={Config['TIMEZONE'].key or 'UTC'}"},
     )
+    globals()["_engine"] = engine
+    globals()["SessionFactory"] = sessionmaker(
+        bind=_engine, expire_on_commit=False, autoflush=False
+    )
+    pg_version = engine.dialect.server_version_info
+    if not pg_version:
+        try:
+            with engine.connect() as conn:
+                version = conn.execute(text("SHOW server_version")).first()
+                pg_version = version and tuple(int(x) for x in version[0].split("."))
+        except SQLAlchemyError:
+            pg_version = (18, 0)  # Assume the minimum supported version if we can't determine it
+    elif pg_version < PG_MINIMUM_VERSION:
+        raise RuntimeError(
+            f"PostgreSQL version {pg_version} is not supported. Upgrade to a newer version to use this application."
+        )
+    return engine
 
 
 def get_session():
+    if not SessionFactory:
+        raise RuntimeError(
+            "Unexpected error, the lifespan for the session has not been initialized"
+        )
     try:
         with SessionFactory.begin() as session:
             yield session
--- api/main.py
+++ api/main.py
@@ -1,9 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.gzip import GZipMiddleware
-from uvicorn import run
 
-from .config import Server
+from .database import create_factories
+from .logger import Logger
 from .routes import v1_router
+
+
+@asynccontextmanager
+async def spam(_):
+    engine = create_factories()
+    yield {"logger": Logger}
+    engine.dispose()
+
 
 __version__ = "0.1.0"
 version = __version__  # Expose version for use in API metadata and elsewhere
@@ -15,9 +25,10 @@ app = FastAPI(
         "where we can identify the host by its fqdn and the event by its name, "
         "storing the timestamp and additional metadata for the service that trigger the event"
     ),
+    lifespan=spam,
 )
 
-app.add_middleware(GZipMiddleware, minimum_size=1024)  # Compress responses larger than 1KB
+app.add_middleware(GZipMiddleware, minimum_size=1024 * 10)  # Compress responses larger than 1MB
 
 
 @app.get("/health", tags=["health"])
@@ -27,7 +38,3 @@ def health():
 
 
 app.include_router(v1_router, prefix="/api", tags=["api", "v1"])
-
-
-if __name__ == "__main__":
-    run(app, **Server, server_header=False)
